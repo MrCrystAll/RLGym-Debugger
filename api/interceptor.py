cfb2dfe71f953da80141dcc5ca5f694a96415b77
Mathieu Suchet <msuchet63@gmail.com>
2026-03-05 11:54:19 +0100

    Add API and basic reward printer

---
+++ api/interceptor.py
@@ -0,0 +1,70 @@
+from abc import abstractmethod
+from typing import Any, Generic
+
+from rlgym.api import AgentID, StateType, ObsType, RewardType
+
+
+class Interceptor(Generic[AgentID, StateType, ObsType, RewardType]):
+    """The base class to intercept and act on the environment"""
+
+    @abstractmethod
+    def reset(
+        self,
+        agents: list[AgentID],
+        initial_state: StateType,
+        shared_info: dict[str, Any],
+    ):
+        """Function to be called each time the environment is reset. Note that this does not need to return anything,
+        the environment will call `build_obs` automatically after reset, so the initial observation for a policy will be
+        constructed in the same way as every other observation.
+
+        :param agents: List of AgentIDs for which this ObsBuilder will return an Obs
+        :type agents: list[AgentID]
+        :param initial_state: The initial game state of the reset environment.
+        :type initial_state: StateType
+        :param shared_info: A dictionary with shared information across all config objects.
+        :type shared_info: dict[str, Any]
+        """
+
+    @abstractmethod
+    def intercept(
+        self,
+        observations: dict[AgentID, ObsType],
+        next_observations: dict[AgentID, Any],
+        rewards: dict[AgentID, RewardType],
+        terminated: dict[AgentID, bool],
+        truncated: dict[AgentID, bool],
+        shared_info: dict[str, Any],
+    ):
+        """The function intercepts and allows the modification of the given arguments
+
+        :param observations: The observations returned by the environment **before** stepping
+        :type observations: dict[AgentID, ObsType]
+        :param next_observations: The observations returned by the environment **after** stepping
+        :type next_observations: dict[AgentID, ObsType]
+        :param rewards: The rewards returned by the environment
+        :type rewards: dict[AgentID, RewardType]
+        :param terminated: The termination signals returned by the environment
+        :type terminated: dict[AgentID, bool]
+        :param truncated: The truncation signals returned by the environment
+        :type truncated: dict[AgentID, bool]
+        :param shared_info: The shared information of the environment
+        :type shared_info: dict[str, Any]
+        """
+
+    @abstractmethod
+    def intercept_actions(
+        self,
+        observations: dict[AgentID, ObsType],
+        actions: dict[AgentID, ObsType],
+        shared_info: dict[str, Any],
+    ):
+        """The function intercepts the actions and the observations used to build the actions before being sent to the env.step method
+
+        :param observations: The observations used to build the action
+        :type observations: dict[AgentID, ObsType]
+        :param actions: The actions themselves
+        :type actions: dict[AgentID, ObsType]
+        :param shared_info: The shared info of the environment
+        :type shared_info: dict[str, Any]
+        """
--- main.py
+++ main.py
@@ -1,45 +1,72 @@
 import random
+import time
 
 from rlgym.api import RLGym
 
 from rlgym.rocket_league.obs_builders import DefaultObs
 from rlgym.rocket_league.action_parsers import LookupTableAction, RepeatAction
 from rlgym.rocket_league.reward_functions import TouchReward
-from rlgym.rocket_league.state_mutators import FixedTeamSizeMutator, MutatorSequence, KickoffMutator
+from rlgym.rocket_league.state_mutators import (
+    FixedTeamSizeMutator,
+    MutatorSequence,
+    KickoffMutator,
+)
 from rlgym.rocket_league.done_conditions import NoopCondition, GoalCondition
 from rlgym.rocket_league.sim import RocketSimEngine
 
+from rlgym_tools.rocket_league.renderers.rocketsimvis_renderer import RocketSimVisRenderer
+
 import numpy as np
 
+from reward_printing.reward_printer import RewardPrinter
+
 if __name__ == "__main__":
+    tick_skip = 1
+    
     env = RLGym(
         obs_builder=DefaultObs(),
-        action_parser=RepeatAction(LookupTableAction()),
+        action_parser=RepeatAction(LookupTableAction(), repeats=tick_skip),
         state_mutator=MutatorSequence(FixedTeamSizeMutator(1, 1), KickoffMutator()),
         reward_fn=TouchReward(),
         termination_cond=GoalCondition(),
         truncation_cond=NoopCondition(),
-        transition_engine=RocketSimEngine()
+        transition_engine=RocketSimEngine(),
+        renderer=RocketSimVisRenderer()
     )
-    
+
     running = True
-    
+    render = True
+
+    reward_printer = RewardPrinter()
+
     print("Starting environment")
     while running:
         try:
             obs = env.reset()
-            
+            reward_printer.reset(env.agents, env.state, env.shared_info)
+
             truncated = {agent: False for agent in env.agents}
             terminated = {agent: False for agent in env.agents}
-            
+
             while not (any(truncated.values()) or any(terminated.values())):
-                actions = {agent: np.asarray([random.randint(0, 89)]) for agent in env.agents}
+                if render:
+                    env.render()
+                    time.sleep(tick_skip / 120.0)
                 
-                next_obs, rewards, terminated, truncated = env.step(actions)
+                actions = {
+                    agent: np.asarray([random.randint(0, 89)]) for agent in env.agents
+                }
+                
+                reward_printer.intercept_actions(obs, actions, env.shared_info)
+
+                env_return = env.step(actions)
+                reward_printer.intercept(obs, *env_return, env.shared_info)
+                
+                obs = env_return[0]
         except KeyboardInterrupt:
             print("Interruption detected")
             running = False
             break
-        
+
     env.close()
     print("Ending")
